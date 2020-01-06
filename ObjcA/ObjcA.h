3393e3723257057150b07c1e7b46724a9a2aad2b
Hironori Ichimiya <hiron@hironytic.com>
2020-01-06 12:50:35 +0900

    Fix error by umbrella-like header

---
+++ ObjcA/ObjcA.h
@@ -0,0 +1,12 @@
+//
+//  ObjcA.h
+//  StaticAgeruyo
+//
+//  Created by 一宮 浩教 on 2020/01/06.
+//  Copyright © 2020 Hironytic. All rights reserved.
+//
+
+#import "Inner/Deep/HNTDeep.h"
+#import "Inner/HNTInnerHello.h"
+#import "HNTHello.h"
+#import "HNTUtility.h"
